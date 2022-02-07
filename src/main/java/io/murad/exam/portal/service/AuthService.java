3388c1e5b5609c5ab7d524b5178dceecf8b4dc77
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-02-08 00:02:43 +0600

    AuthService added

---
+++ src/main/java/io/murad/exam/portal/service/AuthService.java
@@ -0,0 +1,35 @@
+package io.murad.exam.portal.service;
+
+import io.murad.exam.portal.model.AuthenticationRequest;
+import io.murad.exam.portal.model.AuthenticationResponse;
+import io.murad.exam.portal.model.User;
+import io.murad.exam.portal.security.JWTAuthenticationProvider;
+import io.murad.exam.portal.service.impl.UserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+    private UserDetailsServiceImpl userDetailsService;
+    private JWTAuthenticationProvider jwtAuthenticationProvider;
+    private AuthenticationManager authenticationManager;
+
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
+
+    private AuthenticationResponse generateToken(AuthenticationRequest authenticationRequest) {
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        String token = jwtAuthenticationProvider.generateToken(userDetails);
+        return AuthenticationResponse.builder()
+                .authenticationToken(token)
+                .build();
+
+    }
+
+}
