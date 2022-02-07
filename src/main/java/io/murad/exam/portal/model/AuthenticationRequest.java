6ad123c2d71b75ae676b4fa7425559aefb20b287
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-02-07 22:59:11 +0600

    AuthenticationRequest and AuthenticatinResponse added

---
+++ src/main/java/io/murad/exam/portal/model/AuthenticationRequest.java
@@ -0,0 +1,13 @@
+package io.murad.exam.portal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/io/murad/exam/portal/model/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package io.murad.exam.portal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String authenticationToken;
+}
