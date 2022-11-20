e3e9b32a27be72b2a61ad17413d385af6fdfe704
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-11-20 20:13:43 +0600

    category crud implemented

--- src/main/java/io/murad/exam/portal/controller/CategoryController.java
+++ src/main/java/io/murad/exam/portal/controller/CategoryController.java
@@ -1,20 +1,18 @@
 package io.murad.exam.portal.controller;
 
 import io.murad.exam.portal.model.Category;
-import io.murad.exam.portal.model.Question;
-import io.murad.exam.portal.model.Quiz;
 import io.murad.exam.portal.service.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.Set;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/category")
+@CrossOrigin("*")
 public class CategoryController {
     private CategoryService categoryService;
 
